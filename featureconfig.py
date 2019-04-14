6c660563733377d63a97363b38af0eb3e06c490e
rajit-chatterjee <49617495+rajit-chatterjee@users.noreply.github.com>
2019-04-14 22:05:33 +0530

    Add files via upload

--- featureconfig.py
+++ featureconfig.py
@@ -5,10 +5,10 @@ Change this file according to the need to test configurations.
 '''
 
 browser_details={'browser_name':"Chrome",
-                 'browser_path':"C:\DRIVERS\chromedriver.exe",
+                 'browser_path':"<browser path>",
                  "url":"https://www.flipkart.com/"}
 
-user_login_details={"username":"chatterjeerajit007@gmail.com", "password":"R@jit1994!"}
+user_login_details={"username":"<email address>", "password":"<password>"}
 
 elements_to_access={"Electronics_element":"//span[text()='Electronics']",
                     "Mobile_submenu_element":"//a[@href='/mobile-phones-store?otracker=nmenu_sub_Electronics_0_Mobiles']"}
@@ -23,6 +23,6 @@ product_search={"phone":"iphone 7"}
 
 find_product={"product_apple":"Apple iPhone 7"}
 
-user_address_details={"zip_code":"560100"}
+user_address_details={"zip_code":"<zipcode>"}
 
-snap_shot_save_path={"path":"C:\\Users\\rajchatt\\Downloads\\rajit_ss.png"}
+snap_shot_save_path={"path":"<screen shot path>"}
